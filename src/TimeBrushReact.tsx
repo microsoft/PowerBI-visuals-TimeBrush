8ad85478debb97ec9e459278e386c093999f5c13
darthtrevino <darthtrevino@gmail.com>
2016-04-28 17:06:16 -0700

    Pump Dimension Changes into the TimeBrush Component

--- src/TimeBrushReact.tsx
+++ src/TimeBrushReact.tsx
@@ -8,7 +8,7 @@ require("./css/TimeBrush.scss");
 export interface TimeBrushProps {
     selectedRange?: [Date, Date];
     onSelectedRangeChanged?: (range: [Date, Date]) => void;
-    data?: TimeBrushDataItem[],
+    data?: TimeBrushDataItem[];
     width?: number;
     height?: number;
 };
@@ -19,38 +19,42 @@ export interface TimeBrushState { }
  * Thin wrapper around TimeBrush
  */
 export class TimeBrush extends React.Component<TimeBrushProps, TimeBrushState> {
+    public props: TimeBrushProps = {};
     private timeBrush: TimeBrushImpl;
     private node: any;
-    public props : TimeBrushProps = {};
 
-    componentDidMount() {
+    public componentDidMount(): void {
         this.node = ReactDOM.findDOMNode(this);
-        this.timeBrush = new TimeBrushImpl($(this.node), {width: this.props.width, height: this.props.height});+
-        this.timeBrush.events.on("rangeSelected", (range) => {            
-            if (this.props) {
+        this.timeBrush = new TimeBrushImpl($(this.node), {width: this.props.width, height: this.props.height});
+        this.timeBrush.events.on("rangeSelected", (range) => {
+            if (this.props && this.props.onSelectedRangeChanged) {
                 this.props.onSelectedRangeChanged(range);
             }
         });
         this.renderContent();
     }
 
-    componentWillReceiveProps(newProps : TimeBrushProps) {
+    public componentWillReceiveProps(newProps: TimeBrushProps): void {
         this.renderContent(newProps);
     }
 
     /**
      * Renders this component
      */
-    render() {
-        return <div style={{width:"100%", height:"100%"}}></div>;
+    public render(): JSX.Element {
+        return <div className="timeBrushContainer"></div>;
     }
 
-    private renderContent(props? : TimeBrushProps) {
+    private renderContent(props?: TimeBrushProps): void {
         // if called from `componentWillReceiveProps`, then we use the new
         // props, otherwise use what we already have.
         props = props || this.props;
 
         this.timeBrush.data = props.data || [];
         this.timeBrush.selectedRange = props.selectedRange || null;
+        this.timeBrush.dimensions = {
+            width: this.props.width,
+            height: this.props.height,
+        };
     }
 }
